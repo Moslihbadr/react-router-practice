9b841fcffd09ce5af714f4c4a164b1a1d21ca66a
Badr <moslihbadr2@gmail.com>
2023-07-29 13:13:32 +0100

    add the necessary components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
+
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      {/* <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter> */}
     </div>
   );
 }
